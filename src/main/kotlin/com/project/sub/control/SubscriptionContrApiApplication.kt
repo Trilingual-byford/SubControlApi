5972d3570c4d1fd1d35089106c3b1f423a4440df
BeYOND <niconicocsc@gmail.com>
2024-01-16 12:17:53 +0900

    Initial commit.

---
+++ src/main/kotlin/com/project/sub/control/SubscriptionContrApiApplication.kt
@@ -0,0 +1,11 @@
+package com.project.sub.control
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SubscriptionContrApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SubscriptionContrApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/project/sub/control/SubscriptionContrApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.sub.control
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SubscriptionContrApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
